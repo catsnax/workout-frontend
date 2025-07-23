265676e90af74895174b976a1f7a0cbcf59a2131
catsnax <107341849+catsnax@users.noreply.github.com>
2025-07-23 11:48:03 +0800

    added workout form for adding exercises in workout

---
+++ src/components/AddExerciseForm.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+type AddExerciseFormProps = {
+  SK: string;
+};
+
+export default function AddExerciseForm({ SK }: AddExerciseFormProps) {
+  const queryClient = useQueryClient();
+  console.log(SK);
+
+  const mutation = useMutation({
+    mutationFn: (newWorkout: {
+      PK: string;
+      exerciseName: string;
+      numberOfSets: string;
+      weight: string;
+      unitMeasurement: string;
+    }) =>
+      axios.post(
+        "https://rntibe12r1.execute-api.us-east-1.amazonaws.com/exercises",
+        newWorkout
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["exercises"] });
+    },
+  });
+
+  const [PK] = useState(SK);
+  const [exerciseName, setExerciseName] = useState("");
+  const [numberOfSets, setNumberOfSets] = useState("");
+  const [weight, setWeight] = useState("");
+  const [unitMeasurement, setUnitMeasurement] = useState("kg");
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="text-lg text-black font-semibold mb-4">Add New Workout</h2>
+
+      {/*}
+      <select
+        className="border-2 p-2 border-black text-black mb-2 w-1/2"
+        value={targetDay}
+        onChange={(e) => setTargetDay(e.target.value)}
+      >
+        <option value="full">Full Body</option>
+        <option value="upper">Upper Body</option>
+        <option value="lower">Lower Body</option>
+        <option value="Push">Push</option>
+        <option value="Pull">Pull</option>
+        <option value="Legs">Legs</option>
+        <option value="Arms">Arms</option>
+      </select>
+    */}
+      <input
+        className="border-2 p-2 border-black text-black mb-2 w-1/2"
+        placeholder="Exercise Name"
+        value={exerciseName}
+        onChange={(e) => setExerciseName(e.target.value)}
+      />
+      <input
+        className="border-2 p-2 border-black text-black mb-2 w-1/2"
+        placeholder="Number of Sets"
+        value={numberOfSets}
+        onChange={(e) => setNumberOfSets(e.target.value)}
+      />
+      <div className="flex justify-between w-1/2 mb-2 gap-2">
+        <input
+          className="border-2 p-2 h-12 border-black text-black mb-2 w-1/2"
+          placeholder="Weight"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        <select
+          className="border-2 p-2 h-12 border-black text-black w-1/2"
+          onChange={(e) => setUnitMeasurement(e.target.value)}
+        >
+          <option value="kg">Kilograms</option>
+          <option value="lbs">Pounds</option>
+        </select>
+      </div>
+      <button
+        className="text-black"
+        onClick={() => {
+          mutation.mutate({
+            PK,
+            exerciseName,
+            numberOfSets,
+            weight,
+            unitMeasurement,
+          });
+        }}
+      >
+        {" "}
+        Create New Exercise
+      </button>
+    </div>
+  );
+}
--- src/pages/ViewWorkout.tsx
+++ src/pages/ViewWorkout.tsx
@@ -1,12 +1,37 @@
 import { useDataStore } from "../store/dataStore";
+import Modal from "../components/Modal.tsx";
+import { useModalStore } from "../store/modalStore";
+import AddExerciseForm from "../components/AddExerciseForm";
 
 function ViewWorkout() {
+  const open = useModalStore((state) => state.open);
   const sharedData = useDataStore((state) => state.sharedData);
+  const handleAddExercise = () => {
+    open(<AddExerciseForm SK={sharedData?.SK} />);
+  };
+
   console.log("Shared Data:", sharedData);
   return (
     <div className="w-full h-full flex justify-center items-center  flex-col">
+      <div> </div>
+      <Modal />
       <h1 className="text-2xl font-bold mb-4">View Workout</h1>
-      <button>Add New Exercise</button>
+      <button onClick={handleAddExercise}>Add New Exercise</button>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="border px-4 py-2">Exercise</th>
+            <th className="border px-4 py-2">Sets</th>
+            <th className="border px-4 py-2">Weight</th>
+            <th className="border px-4 py-2">Unit Measurement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
